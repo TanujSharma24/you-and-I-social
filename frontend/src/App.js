0bfd8da5f016524a38d3f798b2cc5ef18fda5f62
TanujSharma24 <tansharma@hotmail.com>
2022-11-06 20:22:03 -0700

    installed dependencies
    added tailwind and others

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+// import Login from './components/Login';
+// import Home from './container/Home';
+
+const App = () => {
+  return (
+    // // <Routes>
+    //   {/* <Route path="login" element={<Login />} />
+    //   <Route path="/*" element={<Home />} /> */}
+    // {/* </Routes> */}
+    <h1 className="text-3x1 font-bold underline">Hello</h1>
+
+  );
+};
+
+export default App;
