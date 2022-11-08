51a7a569157df1c2d4159c79f3fe60a553e14e98
TanujSharma24 <tansharma@hotmail.com>
2022-11-08 00:11:55 -0700

    installed dependencies
    added tailwind and others

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
 import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
-// import Login from './components/Login';
-// import Home from './container/Home';
+import Login from './components/Login';
+import Home from './container/Home';
 
 const App = () => {
-  return (
-    // // <Routes>
-    //   {/* <Route path="login" element={<Login />} />
-    //   <Route path="/*" element={<Home />} /> */}
-    // {/* </Routes> */}
-    <h1 className="text-3x1 font-bold underline">Hello</h1>
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    if (!User) navigate('/login');
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   );
 };
 
